f23e95bc75a9f51fd8a6000161fd50dcd160c269
4udiwe <arsenijsarifkulov2633@gmail.com>
2025-03-09 20:51:29 +0300

    added search func and PUT endpoint

--- main.go
+++ main.go
@@ -6,17 +6,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
+type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
+type SearchError struct {
+	Message string
+}
+
+func (e *SearchError) Error() string {
+	return e.Message
+}
+
+var albums = []Album{
 	{ID: "1", Title: "first", Artist: "first artist", Price: 100.50},
 	{ID: "2", Title: "second", Artist: "second artist", Price: 200.50},
 	{ID: "3", Title: "third", Artist: "third artist", Price: 300.50},
+}
+
+func searchAlbumById(id string) (int, *Album, error) {
+	for i, album := range albums {
+		if id == album.ID {
+			return i, &albums[i], nil
+		}
+	}
+	return -1, nil, &SearchError{Message: "album not found"}
 }
 
 func getAlbums(c *gin.Context) {
@@ -24,7 +41,7 @@ func getAlbums(c *gin.Context) {
 }
 
 func postAlbum(c *gin.Context) {
-	var na album
+	var na Album
 	if err := c.BindJSON(&na); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, na)
 		return
@@ -38,13 +55,12 @@ func findAlbum(c *gin.Context) {
 
 	param := c.Param("id")
 
-	for _, album := range albums {
-		if album.ID == param {
-			c.IndentedJSON(http.StatusFound, album)
-			return
-		}
+	_, album, err := searchAlbumById(param)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, param)
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, param)
+	c.IndentedJSON(http.StatusFound, album)
 }
 
 func deleteAlbumById(c *gin.Context) {
@@ -60,6 +76,25 @@ func deleteAlbumById(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, param)
 }
 
+func updateAlbumById(c *gin.Context) {
+	param := c.Param("id")
+
+	index, album, err := searchAlbumById(param)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, param)
+		return
+	}
+
+	if err := c.BindJSON(album); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	albums[index] = *album
+
+	c.IndentedJSON(http.StatusAccepted, album)
+}
+
 func main() {
 	router := gin.Default()
 
@@ -67,6 +102,7 @@ func main() {
 	router.GET("/albums/:id", findAlbum)
 	router.POST("/albums", postAlbum)
 	router.DELETE("/albums/:id", deleteAlbumById)
+	router.PUT("/albums/:id", updateAlbumById)
 
 	router.Run("localhost:8080")
 }
